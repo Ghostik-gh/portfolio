9fa8a590724699a1d7ab5862706d9ab396bb501c
Ghostik-gh <feodor200@mail.ru>
2022-08-27 13:22:40 +0300

    my road map

---
+++ src/pages/RoadMapPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function RoadMapPage() {
+  return (
+    <>
+      <img
+        src="https://github.com/Ghostik-gh/portfolio/blob/master/src/static/roadmap.png?raw=true"
+        alt="Road Map"
+      />
+    </>
+  );
+}
+
+export default RoadMapPage;
