cb2eeeb719a6b67ef79e6949dcf8f87753dfb8d3
Ghostik-gh <feodor200@mail.ru>
2022-08-27 17:04:02 +0300

    node sass | add normalizer

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import App from './App';
-import './index.css';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
