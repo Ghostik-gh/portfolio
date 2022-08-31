fc6ded61d92e354a7e48f038cd92cbd2154dec6f
Ghostik-gh <feodor200@mail.ru>
2022-08-31 15:11:30 +0300

    aboutPage

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,18 @@ import { LeftArrow } from '../components/LeftArrow';
 
 function About() {
   return (
-    <div className="container">
+    <div>
       <LeftArrow to={'/projects'} />
-      <div className="container">About</div>
+      <div className="">
+        <h1 style={{ 'text-align': 'center' }}>My skills on Road Map</h1>
+        <div style={{ 'text-align': 'center' }}>
+          now I'm start learning TypeScript
+        </div>
+        <img
+          src="https://github.com/Ghostik-gh/portfolio/blob/master/src/static/roadmap.webp?raw=true"
+          alt="Road Map"
+        />
+      </div>
     </div>
   );
 }
