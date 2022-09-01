27e86d4603a4d2ed1429a81203e8e1709e06e1a6
Ghostik-gh <feodor200@mail.ru>
2022-09-01 21:02:12 +0300

    about & login pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ function About() {
       <div className="">
         <h1 style={{ 'text-align': 'center' }}>My skills on Road Map</h1>
         <div style={{ 'text-align': 'center' }}>
-          now I'm start learning TypeScript
+          {/* now I'm start learning TypeScript */}
         </div>
         <img
           src="https://github.com/Ghostik-gh/portfolio/blob/master/src/static/roadmap.webp?raw=true"
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { action_login_user } from '../store/ActionCreatorUser';
-
+import './Login.scss';
 function Login() {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
@@ -20,20 +20,28 @@ function Login() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const form = event.target;
-    const user = form.username?.value || 'ok, stay unknown';
-    logIn(user);
+    const user = form.username?.value || null;
+    if (user !== null) {
+      logIn(user);
+    } else {
+      alert('please input name');
+    }
   };
 
   return (
     <div className="container">
-      <div>Login: {user.name}!</div>
-      {fromPage === '/about' ? <>Enter your name to see AboutPage</> : null}
-      <form onSubmit={handleSubmit}>
-        <label>
-          <input type="text" placeholder="input name" name="username" />
-        </label>
-        <button className="btn btn-dark">Autorization</button>
-      </form>
+      <div className="login-text">
+        {user.name !== 'Unknown' ? <div>Hello: {user.name}!</div> : null}
+        {fromPage === '/about' || '/portfolio' ? (
+          <div className="">Enter your name to view information about me</div>
+        ) : null}
+        <form onSubmit={handleSubmit}>
+          <label>
+            <input type="text" placeholder="input name" name="username" />
+          </label>
+          <button className="btn btn-dark">Autorization</button>
+        </form>
+      </div>
     </div>
   );
 }
